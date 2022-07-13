72a5934c9b9ece59d8c140df754a494ad784e65b
«Asya» <talantsevaa@bk.ru>
2022-07-14 00:24:35 +0300

    Нахождение максимального и минимального из двух чисел

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,5 +1,13 @@
-﻿int a = 5;
-int b = 7; 
+﻿//int a = 5;
+//int b = 7; 
+
+int a = new Random().Next(1,10);
+
+Console.WriteLine(a);
+
+int b = new Random().Next(1,10);
+
+Console.WriteLine(b);
 
 int max = a;
 
