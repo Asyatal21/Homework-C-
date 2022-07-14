f2fae1e942c2b910c005cbf53f4aad87c27ccf11
«Asya» <talantsevaa@bk.ru>
2022-07-14 23:50:23 +0300

    Сравнение двух чисел

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,13 +1,21 @@
 ﻿//int a = 5;
 //int b = 7; 
 
-int a = new Random().Next(1,10);
+//int a = new Random().Next(1,10);
 
-Console.WriteLine(a);
+//Console.WriteLine(a);
 
-int b = new Random().Next(1,10);
+//int b = new Random().Next(1,10);
 
-Console.WriteLine(b);
+//Console.WriteLine(b);
+
+Console.WriteLine("Введите число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число");
+
+int b = Convert.ToInt32(Console.ReadLine());
 
 int max = a;
 
