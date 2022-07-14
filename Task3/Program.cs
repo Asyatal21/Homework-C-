4db41d4b28c1a5396cb6d7ff92cc5b45418c4afb
«Asya» <talantsevaa@bk.ru>
2022-07-14 23:19:03 +0300

    Определение четное или нечетное

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int numberA = new Random().Next(1,10);
+
+Console.WriteLine(numberA);
+
+if (numberA % 2 == 0)
+
+{
+
+Console.WriteLine("четное");
+
+}
+
+
+
+else
+
+{
+ Console.WriteLine("нечетное");   
+}
+
+
