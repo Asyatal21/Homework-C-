42b2f5cc0a038d585c6bdcf25bfecb8bbcbb4eca
«Asya» <talantsevaa@bk.ru>
2022-07-19 17:41:00 +0300

    Вторая цифра трехзначного числа

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// На вход трехзначное число, на выходе вторая цифра этого числа
+
+Console.WriteLine("Введите число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+
+int b =  a / 10 % 10;
+
+int c = b % 10;
+
+Console.WriteLine(c);
