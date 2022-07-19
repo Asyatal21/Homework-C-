ae55f533a0919bc1b8ed9e2ae3373462be344f3c
«Asya» <talantsevaa@bk.ru>
2022-07-19 17:42:21 +0300

    Третье число любого числа

---
+++ Task13/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+
+{
+if (x >= 6)
+{
+    Console.WriteLine("Выходной");
+}
+if (x == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (x == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (x == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (x == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (x == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (x == 6)
+{
+    Console.WriteLine("суббота");
+}
+if (x == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
+else 
+{
+    Console.WriteLine("Не выходной");
+}
+}
