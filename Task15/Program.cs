5e85c0194f86ab0193f3164eb0f59aa0833e18b0
«Asya» <talantsevaa@bk.ru>
2022-07-19 17:43:28 +0300

    Выходной или не выходной день недели

---
+++ Task15/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число");
+
+string number = Console.ReadLine();
+
+if (number.Length > 2)
+{
+    Console.WriteLine(number[2]);
+
+}
+
+else
+
+{   
+    Console.WriteLine("Третьей цифры нет");
+
+}
+
+
+
+
