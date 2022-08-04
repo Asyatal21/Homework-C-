0871ca3412658226aae045f4d3bb2712add6f8dd
«Asya» <talantsevaa@bk.ru>
2022-08-04 17:26:24 +0300

    Таблица кубов

---
+++ Task23/Program.cs
@@ -0,0 +1,11 @@
+﻿// На вход число от 1 до N, а на выходе таблица кубов от 1 до N
+Console.WriteLine("Введите число");
+int a =  Convert.ToInt32(Console.ReadLine());
+int b = 1;
+while(b <= a)
+{
+    Console.WriteLine(b*b*b);
+    b = b+1; 
+}
+
+
