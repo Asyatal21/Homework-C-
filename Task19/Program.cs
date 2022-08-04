2727109520b7fdd6679ccf8ecd1c8df12b4c86a2
«Asya» <talantsevaa@bk.ru>
2022-08-04 17:22:16 +0300

    Палиндром

---
+++ Task19/Program.cs
@@ -0,0 +1,54 @@
+﻿// На вход пятизначное число и проверить является ли оно палиндромом
+
+//Console.WriteLine("Введите число  ");
+
+//string s = Console.ReadLine();
+
+
+
+
+//if (s[1] = s[5] && s[2] = s[4])
+//{
+   // Console.WriteLine("Является палиндромом");
+//} 
+
+
+
+
+Console.WriteLine("Введите число  ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] myArray = new int[N]; 
+if (myArray[0] = myArray[4])
+
+{
+    if (myArray[1] = myArray[3])
+    {
+        Console.WriteLine("Является палиндромом");
+   }
+    else
+    {
+        Console.WriteLine("Не является палиндромом");
+    }
+
+}
+
+//Console.WriteLine("Введите число");
+///double [] array = new int[5];
+
+//if (array[0] = array[4])
+
+//{
+   // if (array[1] = array[3])
+   // {
+     //   Console.WriteLine("Является палиндромом");
+   // }
+    //else
+   // {
+   //     Console.WriteLine("Не является палиндромом");
+    //}
+    //Console.WriteLine("Является палиндромом");
+//}
+//else
+//{
+    ////Console.WriteLine("Не является палиндромом");
+///}
