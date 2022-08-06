a4ae486ef97c61ed453406d3e007bf1ee6aed8a1
«Asya» <talantsevaa@bk.ru>
2022-08-07 00:25:35 +0300

    Сумма цифр

---
+++ Task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход число и выдает сумму цифр в числе
+
+Console.WriteLine("Введите число");
+
+string number = Console.ReadLine();
+
+int length = number.Length;
+
+int summa = 0;
+
+
+for (int i = 0; i<length;i++)
+
+{
+    summa =  summa+int.Parse(number[i].ToString());
+}
+
+Console.WriteLine (summa);
+
+
+
