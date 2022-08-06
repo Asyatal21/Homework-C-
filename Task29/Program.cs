34e3d9b1ba7f13108f33bccdf50d7f71aafcff86
«Asya» <talantsevaa@bk.ru>
2022-08-07 00:26:28 +0300

    Вывод массива

---
+++ Task29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая создает массив из введенных через запятую цифр
+//void PrintArray(int[] col)
+//{
+    //int count = col.Length;
+    //int position = 0;
+    //while (position < count)
+    //{
+       // Console.Write($"{col[position]} ");
+        //position++;
+    //}
+//}
+Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b =  Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("{a,b}");
