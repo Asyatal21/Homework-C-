9bb62b45ae025b2f2acf6272f464f725d1662a49
«Asya» <talantsevaa@bk.ru>
2022-08-17 17:59:36 +0300

    Массив из вещественных чисел

---
+++ Task47/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
+void FillArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-100, 100);
+        }
+    }
+}
+Console.WriteLine("ведите число строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("ведите число число стобцов");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m, n];
+FillArray(array);
+PrintArray(array);
