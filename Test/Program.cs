72948bc97b44a32f67f66c16744f3963a5806523
«Asya» <talantsevaa@bk.ru>
2022-09-10 20:44:05 +0300

    Итоговая работа

---
+++ Test/Program.cs
@@ -0,0 +1,10 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше или равна 3 символам
+string [] array = {"asds","tyuyg","ytr","yh"};
+string res = 0;
+for(int i=0; i<array.Length; i++) 
+
+    if(array.Length[i] >= 3)
+    {
+        res = res;
+    }
+    else res = res + array[i];
